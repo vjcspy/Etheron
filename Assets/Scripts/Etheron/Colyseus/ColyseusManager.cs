ce20f2d0bf7ff3515fdbc84d5fb8ad071bf584d9
win <mr.vjcspy@gmail.com>
2025-04-30 18:41:10 +0700

    chore: implement UI Toolkit

---
+++ Assets/Scripts/Etheron/Colyseus/ColyseusManager.cs
@@ -0,0 +1,61 @@
+﻿using Colyseus;
+using Cysharp.Threading.Tasks;
+using System;
+using UnityEngine;
+namespace Etheron.Colyseus
+{
+    public class ColyseusManager : MonoBehaviour
+    {
+
+        [SerializeField] private string serverEndpoint = "ws://localhost:2567";
+
+        public ColyseusClient client { get; private set; }
+
+        public static ColyseusManager Instance { get; private set; }
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(obj: gameObject);
+                return;
+            }
+            Instance = this;
+            // DontDestroyOnLoad(target: gameObject);
+
+            client = new ColyseusClient(endpoint: serverEndpoint);
+        }
+
+        public async UniTask SignInAsync(string email, string password)
+        {
+            WhenLoading(isLoading: true);
+
+            try
+            {
+                // 1) Gọi SignIn… trả về Task<AuthUser>
+                // 2) Chuyển thành UniTask<AuthUser>
+                // 3) Gán cancellation token của GameObject
+                IAuthData user = await client.Auth
+                    .SignInWithEmailAndPassword(email: email, password: password);
+
+                Debug.Log(message: $"[Auth] Signed in: {user.RawUser}");
+            }
+            catch (OperationCanceledException)
+            {
+                Debug.LogWarning(message: "[Auth] SignIn was canceled.");
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError(message: $"[Auth] SignIn failed: {ex}");
+            }
+            finally
+            {
+                WhenLoading(isLoading: false);
+            }
+        }
+
+        private void WhenLoading(bool isLoading)
+        {
+            Debug.Log(message: $"Loading Colyseus: {isLoading}");
+        }
+    }
+}
---
+++ Assets/Scripts/Etheron/Colyseus/Schemas/Player.cs
@@ -0,0 +1,31 @@
+﻿using Colyseus.Schema;
+namespace Etheron.Colyseus.Schemas
+{
+    public class PlayerContext : Schema
+    {
+        [Type(index: 0, type: "uint8")]
+        public byte State = 0;
+    }
+
+    public class PlayerPosition : Schema
+    {
+        [Type(index: 0, type: "number")]
+        public float X = 0f;
+
+        [Type(index: 1, type: "number")]
+        public float Y = 0f;
+    }
+
+    public class Player : Schema
+    {
+
+        [Type(index: 2, type: "ref", childType: typeof(PlayerContext))]
+        public PlayerContext Context = new PlayerContext();
+
+        [Type(index: 0, type: "string")]
+        public string Id = "";
+
+        [Type(index: 1, type: "ref", childType: typeof(PlayerPosition))]
+        public PlayerPosition Position = new PlayerPosition();
+    }
+}
