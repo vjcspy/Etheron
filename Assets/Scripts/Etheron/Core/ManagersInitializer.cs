c598fa3b153f987876322ef0d2d4846ff2d0b36d
win <mr.vjcspy@gmail.com>
2025-05-01 10:21:16 +0700

    feat: implement the strategy to manage manager classes

---
+++ Assets/Scripts/Etheron/Core/ManagersInitializer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+namespace Etheron.Core
+{
+    public class ManagersInitializer : MonoBehaviour
+    {
+        private static bool hasInitialized;
+
+        [SerializeField] private List<GameObject> managerPrefabs;
+
+        private void Awake()
+        {
+            if (hasInitialized)
+            {
+                Destroy(obj: gameObject);
+                return;
+            }
+
+            hasInitialized = true;
+
+            foreach (GameObject prefab in managerPrefabs)
+            {
+                GameObject instance = Instantiate(original: prefab);
+                DontDestroyOnLoad(target: instance);
+            }
+
+            DontDestroyOnLoad(target: gameObject);
+        }
+    }
+}
