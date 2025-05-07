0998d0109dbd5b8a2513ed7aaf8787d629072731
win <mr.vjcspy@gmail.com>
2025-05-07 20:14:51 +0700

    feat: update CatGirlServer prefab layer and reduce update interval; add ThreadSafeStructValue utility

---
+++ Assets/Scripts/Etheron/Utils/Thread/ThreadSafeValue.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+namespace Etheron.Utils.Thread
+{
+    // Object wrapper class cho struct
+    public class ThreadSafeStructValue<T> where T : struct
+    {
+        private volatile Wrapper wrapper = new Wrapper(); // Initialize wrapper
+
+        // Cập nhật giá trị mới cho struct
+        public void Set(T newValue)
+        {
+            Wrapper location = wrapper;
+            location.Value = newValue;
+
+            Volatile.Write(location: ref location, value: location);
+        }
+
+        // Lấy giá trị của struct
+        public T Get()
+        {
+            Wrapper location = wrapper;
+            Wrapper wrapperCopy = Volatile.Read(location: ref location);
+
+            return wrapperCopy.Value;
+        }
+
+        private class Wrapper
+        {
+            public T Value;
+        }
+    }
+}
